e25a61ce7d7c6e5ffbe3dd6d3555ff8bcd617a80
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2018-10-07 15:35:48 +0700

    add attention style

--- src/components/repository-card/index.js
+++ src/components/repository-card/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-
 import styled from "react-emotion";
 
+const onAttention = "&:hover, &:focus";
 const CardStyles = styled.div`
   margin-top: 2rem;
   padding: 1rem;
   border: 1px solid rgba(34, 36, 38, 0.15);
   border-radius: 0.3rem;
+  ${onAttention}: {
+      borderColor: "#96c8da",
+      boxShadow: "0 2px 3px 0 rgba(34,36,38,.15)"
+    }
 `;
 
 export default ({
